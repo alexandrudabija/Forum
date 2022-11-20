75ed892b6a56ca5b26e04214c7eff5ecb6c3a1ef
alexandrudabija <alexandru.dabija1996@gmail.com>
2022-11-21 01:31:43 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forum';
+
+  constructor(private router :  Router )  
+
+{
+  // we force to open  first the index !
+this.router.navigate(['/index']);
+
+}
+
 }
